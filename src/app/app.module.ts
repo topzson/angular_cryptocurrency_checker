a0475552de6685c01edcc86d5ee4d8571c229ad0
Rattatammanoon Prawasuk <toprattatammanoon@hotmail.com>
2023-06-06 15:21:32 +0700

    add banner get gecko api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoinDetailComponent } from './coin-detail/coin-detail.component';
+import { CoinListComponent } from './coin-list/coin-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoinDetailComponent,
+    CoinListComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +28,9 @@ import { AppComponent } from './app.component';
     MatSelectModule,
     MatFormFieldModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    MatPaginatorModule
     
   ],
   providers: [],
--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from '../service/api.service';
 @Component({
   selector: 'app-coin-list',
   templateUrl: './coin-list.component.html',
   styleUrls: ['./coin-list.component.scss']
 })
-export class CoinListComponent {
+export class CoinListComponent implements OnInit{
 
+  bannerData: any=[];
+  dataSource!: MatTableDataSource<any>;
+  displayedColumns: string[] = ['symbol', 'currency_price','price_change_percentage_24h','matket_cap'];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(private api : ApiService) {}
+
+  ngOnInit(): void {
+      this.getAllData();
+      this.getBannerData();
+  }
+  getBannerData(){
+    this.api.getTrendingCurrency("INR").subscribe(res=>{
+      console.log(res);
+      this.bannerData = res;
+    })
+  }
+
+  getAllData(){
+    this.api.getCurrency("INR").subscribe(res=>{
+      console.log(res);
+      this.dataSource = new MatTableDataSource<any>(res);
+      this.dataSource.sort = this.sort;
+    })
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
